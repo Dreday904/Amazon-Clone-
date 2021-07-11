129246eaf193a6ab57d78fe0d2cfb9d20829bd3e
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-07-11 05:00:16 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from './Header';
+
+function App() {
+    return (
+        // BEM
+    <div className="App">
+          <h1>Hello Dre, let's build the
+              Amazon store! </h1>
+            <Header />
+            {/* Home */}
+    </div>
+  );
+}
+
+expor
