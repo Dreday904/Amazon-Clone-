0c57e30a124f3b4816d84aef031a7f3e14e1754f
Dreday <54422312+Dreday904@users.noreply.github.com>
2021-07-13 02:06:27 -0400

    Update Header.js

--- Header.js
+++ Header.js
@@ -1,51 +1,59 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
     function Header() {
         return (
+
             <div className='header'>
                 <img
                     className="header__logo"
-
                     src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" />
-
-                <div
-                    className="header__search">
-                    <input
-                        className="header__searchInput" type="text" />
-                    {/* Logo */}
+                <div className="header__search">
+                         <input
+                        className="header__searchInput"
+                        type="text" />
+                             <SearchIcon
+                        className="header__searchIcon" />
                 </div>
+
 
                 <div className="header__nav">
                     <div className="header__option">
-                    <span
+                        <span
                     className="header__optionLineOne">
-                    Hello Guest</span>
-                    <span
+                        Hello Guest</span>
+                        <span
                     className="header__optionLineTwo">
-                    Sign In</span>
+                         Sign In</span>
                 </div>
 
 
                 <div className="header__option">
-                    <span
+                        <span
                     className="header__optionLineOne">
-                    Returns</span>
-                    <span
+                        Returns</span>
+                        <span
                     className="header__optionLineTwo">
-                    & Orders</span>
+                        & Orders</span>
                </div>
 
 
                <div className="header__option">
-                    <span
+                        <span
                     className="header__optionLineOne">
-                    Your</span>
-                    <span
+                        Your</span>
+                        <span
                     className="header__optionLineTwo">
-                    Prime</span>
+                        Prime</span>
                </div>
 
+                    <div className="header__optionBasket">
+                        <ShoppingCartIcon />
+                        <span className="header__optionLineTwo
+                            header__basketCount">0</span>
+                    </div>
                </div>
             </div>
         );
